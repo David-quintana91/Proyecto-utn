33d910261a8b44601c78ab51563eab924498710c
david-quintana91 <davidquintanagmail.com>
2023-01-29 20:11:57 -0300

    llamada open weather map

---
+++ clima.js
@@ -0,0 +1,3 @@
+var url = "https://api.openweathermap.org/data/2.5/weather?lat=-34.807859085538475&lon=-58.40048983075379&appid=9913f48dd4ffc17400f585dbbb273b27&units=metric&lang=es";
+
+fetch(url);
