4044a09aa45ea9a6d63e9d12ed528b57b462a211
david-quintana91 <davidquintanagmail.com>
2023-01-29 22:31:41 -0300

    continuando con clima

--- clima.js
+++ clima.js
@@ -1,3 +1,17 @@
 var url = "https://api.openweathermap.org/data/2.5/weather?lat=-34.807859085538475&lon=-58.40048983075379&appid=9913f48dd4ffc17400f585dbbb273b27&units=metric&lang=es";
 
-fetch(url);
+fetch(url).then(solicitud =>{
+    console.log(solicitud);
+    if (solicitud.ok== true){
+        console.log("funciono!");
+
+        solicitud.json().then(clima => {
+            console.log(clima)
+        });
+
+
+
+    } else{
+        console.error("Error", solicitud.status, solicitud, solicitud.statusText);
+    }
+});
